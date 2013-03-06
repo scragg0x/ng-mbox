4acd394b050aba503a436f10fdfe58507637fda4
Matthew <scragg@gmail.com>
2013-03-06 17:10:31 -0600

    refactoring

--- mbox.js
+++ mbox.js
@@ -1,8 +1,16 @@
 User = {};
+User.id = 1;
 User.username = "Joe";
 
+Mbox = {};
+Mbox.users = {
+    1: {username: 'Joe'},
+    2: {username: 'Scragg'},
+    3: {username: 'Seminull'}
+};
+
 angular.module('mbox', [], function($routeProvider, $locationProvider) {
-    $routeProvider.when('/:box_name/:conv_id', {
+    $routeProvider.when('/:folder_name/:conv_id', {
         templateUrl: 'conv.html',
         controller: ConvCtrl
     });
@@ -13,39 +21,58 @@ function MboxCtrl($scope, $route, $routeParams, $location) {
     $scope.$location = $location;
     $scope.$routeParams = $routeParams;
 
-    $scope.activeBox = 'Inbox';
+    $scope.active_folder = 1;
 
-    $scope.boxes = [
-        {name: 'Inbox'},
-        {name: 'Sent'},
-        {name: 'Archive'},
-        {name: 'Spam'},
-        {name: 'Trash'}
+    $scope.box = {
+        user_id: User.id,
+        conv_count: 0,
+        msg_count: 0,
+        new_count: 0,
+        spam_count: 0,
+        time_read: 0
+    };
+
+    $scope.folders = [
+        {id: 1, name: 'Inbox'},
+        {id: 2, name: 'Sent'},
+        {id: 3, name: 'Archive'},
+        {id: 4, name: 'Spam'},
+        {id: 5, name: 'Trash'}
     ];
 
     $scope.convs = {
-        Inbox : [
-            {subject: 'Hello', from: 'Scragg', to: 'Joe', msg_count: 2, id: 1},
-            {subject: 'Bro!', from: 'Seminull', to: 'Joe', msg_count: 1, id: 2}
+        1 : [
+            {subject: 'Hello', time_started: 0, time_updated: 0, msg_count: 2, id: 1, unread: 0, users:[1,2]},
+            {subject: 'Bro!', time_started: 0, time_updated: 0, msg_count: 1, id: 2, unread: 1, users:[1,3]}
         ],
-        Sent: [{subject: 'hey!', from: 'Joe', to: 'Mark', msg_count: 1, id: 3}],
-        Archive: [],
-        Spam: [],
-        Trash: []
+        2 : [
+            {subject: 'hey!', time_started: 0, time_updated: 0, msg_count: 1, id: 3, unread: 0, users:[1,2]}
+        ]
     };
 
     $scope.msgs = {
-        1: [{text: 'hello!'}, {text: 'whats up!'}],
-        3: [{text: 'what is your favorite color?'}]
+        1: [
+            {id: 1, msg: 'hello!', time_sent: 0, user_id: 1},
+            {id: 2, msg: 'whats up!', time_sent: 0, user_id: 2}
+        ],
+        3: [{id: 3, msg: 'what is your favorite color?', time_sent: 0, user_id: 2}]
     };
 
-    $scope.activateBox = function(box){
-        $scope.activeBox = box.name;
+    $scope.activateFolder = function(folder){
+        $scope.active_folder = folder.id;
     };
 
-    $scope.addConv = function(box) {
+    $scope.getConvCount = function(folder){
+        if ($scope.convs[folder.id]){
+            return $scope.convs[folder.id].length;
+        } else {
+            return 0;
+        }
+    };
+
+    $scope.addConv = function(folder) {
         var id = random(100);
-        $scope.convs[box].push({
+        $scope.convs[folder.id].push({
             to: $scope.to,
             from: User.username,
             subject: $scope.subject,
@@ -56,19 +83,19 @@ function MboxCtrl($scope, $route, $routeParams, $location) {
         $scope.subject = $scope.text = $scope.to = "";
     };
 
-    $scope.addMsg = function(conv_id, text){
-        $scope.msgs[conv_id] = {text: text};
+    $scope.addMsg = function(conv_id, msg){
+        $scope.msgs[conv_id] = {msg: msg};
     };
 
-    $scope.moveConv = function(from_box, to_box_name){
-        var old = $scope.convs[from_box.name];
-        $scope.convs[from_box.name] = [];
+    $scope.moveConv = function(from_folder, to_folder_id){
+        var old = $scope.convs[from_folder.id];
+        $scope.convs[from_folder.id] = [];
         angular.forEach(old, function(conv){
             if (!conv.selected){
-                $scope.convs[from_box.name].push(conv);
+                $scope.convs[from_folder.id].push(conv);
             } else {
                 conv.selected = false;
-                $scope.convs[to_box_name].push(conv);
+                $scope.convs[to_folder_id].push(conv);
             }
         });
     };
@@ -77,13 +104,18 @@ function MboxCtrl($scope, $route, $routeParams, $location) {
         $location.hash(hash);
     };
 
-    $scope.formatConvName = function(conv, box){
+    $scope.formatConvName = function(conv, folder){
         var r = "";
-        if (box.name === 'Sent'){
-            r += "To: " + conv.to;
-        } else {
-            r += conv.from;
+        if (folder.name === 'Sent'){
+            r += "To: ";
         }
+
+        angular.forEach(conv.users, function(id){
+            if (id != User.id){
+                r += Mbox.users[id].username + " ";
+            }
+        });
+
         if (conv.msg_count > 1){
             r +=  " (" + conv.msg_count + ")";
         }
